dcf5411cb65f0ee8334d1708da152d390ed02e5a
Ankit <s533987@nwmissouri.edu>
2019-09-11 13:44:42 -0500

    Add history to login component

--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -19,7 +19,7 @@ import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Dashboard from './Dashboard';
 import { withStyles } from '@material-ui/core/styles';
-
+import history from '../history'
 
 const useStyles = theme => ({
   '@global': {
@@ -125,6 +125,7 @@ class SignIn extends Component {
     const { classes } = this.props;
   
     if (this.state.isLoggedIn) {
+      history.push('/')
       return <Redirect to={{
         pathname: '/',
         email: { id: '123' }
